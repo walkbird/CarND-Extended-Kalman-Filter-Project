165ff0c84fedacad6cd90065415fc90e3a077072
yicheng zhao <zhaoyic@gmail.com>
2017-10-09 01:37:00 +0800

    add solution

--- src/kalman_filter.cpp
+++ src/kalman_filter.cpp
@@ -25,6 +25,9 @@ void KalmanFilter::Predict() {
   TODO:
     * predict the state
   */
+  x_ = F_ * x_;
+	MatrixXd Ft = F_.transpose();
+	P_ = F_ * P_ * Ft + Q_;
 }
 
 void KalmanFilter::Update(const VectorXd &z) {
@@ -32,6 +35,19 @@ void KalmanFilter::Update(const VectorXd &z) {
   TODO:
     * update the state by using Kalman Filter equations
   */
+  VectorXd z_pred = H_ * x_;
+	VectorXd y = z - z_pred;
+	MatrixXd Ht = H_.transpose();
+	MatrixXd S = H_ * P_ * Ht + R_;
+	MatrixXd Si = S.inverse();
+	MatrixXd PHt = P_ * Ht;
+	MatrixXd K = PHt * Si;
+
+	//new estimate
+	x_ = x_ + (K * y);
+	long x_size = x_.size();
+	MatrixXd I = MatrixXd::Identity(x_size, x_size);
+	P_ = (I - K * H_) * P_;
 }
 
 void KalmanFilter::UpdateEKF(const VectorXd &z) {
@@ -39,4 +55,39 @@ void KalmanFilter::UpdateEKF(const VectorXd &z) {
   TODO:
     * update the state by using Extended Kalman Filter equations
   */
+
+  // z_pred
+  VectorXd z_pred = VectorXd(3);
+
+  //recover state parameters
+  float px = x_(0);
+	float py = x_(1);
+	float vx = x_(2);
+  float vy = x_(3);
+  
+  //pre-compute a set of terms to avoid repeated calculation
+	float c1 = px*px+py*py;
+  float c2 = sqrt(c1);
+  
+  z_pred << c2 , atan2(py, px) , (px*vx+py*vy) / c2;
+
+  VectorXd y = z - z_pred;
+  // keep phi in [-pi,pi]
+  while (y(1) < -pi) {
+    y(1) += 2*pi;
+  }
+  while (y(1) > pi) {
+    y(1) -= 2*pi;
+  }
+	MatrixXd Ht = H_.transpose();
+	MatrixXd S = H_ * P_ * Ht + R_;
+	MatrixXd Si = S.inverse();
+	MatrixXd PHt = P_ * Ht;
+	MatrixXd K = PHt * Si;
+
+	//new estimate
+	x_ = x_ + (K * y);
+	long x_size = x_.size();
+	MatrixXd I = MatrixXd::Identity(x_size, x_size);
+	P_ = (I - K * H_) * P_;
 }
--- src/kalman_filter.h
+++ src/kalman_filter.h
@@ -2,6 +2,8 @@
 #define KALMAN_FILTER_H_
 #include "Eigen/Dense"
 
+const double pi = 3.1415926535897;
+
 class KalmanFilter {
 public:
 
